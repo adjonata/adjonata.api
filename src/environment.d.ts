68548b9b3446a0360b4bb78d8036a2227758ecaa
adjonata <alexdjonata30@gmail.com>
2021-11-08 18:45:51 -0300

    fix: update environment

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -4,7 +4,7 @@ declare global {
       NODE_ENV: "production" | "development";
       CONNECTION: string;
       ENABLE_REGISTRATION: "true" | "false";
-      PRODUCTION_PORT: string;
+      PORT: string;
     }
   }
 }
