2a54427132ff7bd14ea854fc90161f0b20e53831
adjonata <alexdjonata30@gmail.com>
2021-11-08 18:30:10 -0300

    fix: remove https

--- src/server.ts
+++ src/server.ts
@@ -4,25 +4,14 @@ dotenv.config({
   path: __dirname + "/../.env"
 });
 
-import https from "https";
 import mongoose from "mongoose";
 import app from "./app";
-import fs from "fs";
-import path from "path";
 
 function createServer(): void {
   const port = parseInt(process.env.PRODUCTION_PORT);
 
-  const httpsServer = https.createServer(
-    {
-      key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
-      cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem"))
-    },
-    app
-  );
-
-  httpsServer.listen(port, () => {
-    console.log(`Https server running on port ${port}!`);
+  app.listen(port, () => {
+    console.log(`Application runing on port ${port}`);
   });
 }
 
