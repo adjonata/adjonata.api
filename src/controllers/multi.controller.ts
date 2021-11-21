b91b170e2c1a718223ef7e4a7cdf9b7f81bc1672
adjonata <alexdjonata30@gmail.com>
2021-11-21 12:34:38 -0300

    fix: using sort in projects getter inside multi controller

--- src/controllers/multi.controller.ts
+++ src/controllers/multi.controller.ts
@@ -10,7 +10,7 @@ export default {
     try {
       const about = await About.findOne().sort("-createdIn");
       const knowledges = await Knowledges.find();
-      const projects = await Projects.find();
+      const projects = await Projects.find().sort({ spotlight: "desc" });
       const social = await Social.find();
 
       return response.status(200).json({
